ab45c2f827234a372392bf7fb617570ea2fdcd2a
sandy <18382255942@163.com>
2020-03-31 12:58:44 +0800

    fix(readme): fix readme

--- models/init_db.go
+++ models/init_db.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 )
@@ -8,6 +9,18 @@ import (
 var DB *gorm.DB
 
 func InitDB(dsn string) {
+	// // 配置MySQL连接参数
+	// username := "kms_dev"  // 账号
+	// password := "kms_good123" // 密码
+	// host := "rm-j6c49n23e4d07l8ijmo.mysql.rds.aliyuncs.com" // 数据库地址，可以是Ip或者域名
+	// port := 3306 // 数据库端口
+	// Dbname := "eth_kms" // 数据库名
+	//
+	// // 通过前面的数据库参数，拼接MYSQL DSN， 其实就是数据库连接串（数据源名称）
+	// // MYSQL dsn格式： {username}:{password}@tcp({host}:{port})/{Dbname}?charset=utf8&parseTime=True&loc=Local
+	// // 类似{username}使用花括号包着的名字都是需要替换的参数
+	// dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", username, password, host, port, Dbname)
+	fmt.Println("dsn: ", dsn)
 	db, err := gorm.Open("mysql", dsn)
 	if err != nil {
 		panic(err)
